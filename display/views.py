7a20311ad23fda803577372d2401782847149213
ctbirds <ctbirdsas@gmail.com>
2021-04-11 17:47:11 +0200

    add api requests

--- display/views.py
+++ display/views.py
@@ -5,5 +5,26 @@ import requests
 from django.http import HttpResponse
 
 def index(request):
-    show = "text"
-    return render(request, 'index.html', {'variable': show})
+    # Tar inn navn på buss-stopp og henter koordinatene fra Entur/Ruter
+    endpoint_entur = "https://api.entur.io/geocoder/v1/autocomplete?text="
+    bus_stop = "Storo"
+    params_entur = "&size=10&lang=n"
+    url_entur = endpoint_entur + bus_stop + params_entur
+    r = requests.get(url_entur)
+
+    coordinates = r.json()['features'][0]['geometry']['coordinates']
+
+    lat = round(coordinates[1], 4)
+    lon = round(coordinates[0], 4)
+    str_lat = str(lat)
+    str_lon = str(lon)
+
+    # Henter temperaturdata fra Yr for valgte koordinater (maks fire desimaler)
+    url_yr = "https://api.met.no/weatherapi/nowcast/2.0/complete?lat=" + str_lat + "&lon=" + str_lon
+    headers = {"User-Agent": "torsteings torstein.skarsgard@gmail.com"}
+    r = requests.get(url_yr, headers=headers)
+
+    air_temperature = r.json()['properties']['timeseries'][0]['data']['instant']['details']['air_temperature']
+    print("Temperaturen ved", bus_stop, "er", air_temperature, "grader celcius akkurat nå")
+
+    return render(request, 'index.html', {'variable': air_temperature})
